49f289feafbb7ff4f82fb1d56bd19a995d17d31c
StudyKeepYell <skyjin6667@gmail.com>
2025-06-01 15:42:27 -0400

    0.1 test

---
+++ include/engine/Move.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "Square.hpp"
+
+namespace chess::engine{
+
+enum class PieceType {
+	King,
+	Queen,
+	Rook,
+	Bishop,
+	Knight,
+	Pawn,
+	None
+};
+
+enum class Color {
+	White,
+	Black
+};
+
+struct Move {
+	Square from{}, to{};
+	PieceType promotion { PieceType::None };
+	bool	  isEnPassant{ false};
+	bool	  isCastle{ false};
+};
+};
+//namespace chess::engine
