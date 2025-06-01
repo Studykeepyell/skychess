bd48ba186e13813a4fce3e563072c0ad0a5c3db8
StudyKeepYell <skyjin6667@gmail.com>
2025-06-01 15:43:00 -0400

    0.1 test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
-//Skychess
+//Skychess main
+#include <SFML/Graphics.hpp>
+#include "../include/engine/Board.hpp"
+#include "../include/gui/Renderer.hpp"
+
+
 int main(){
 
-	std::cout<<"skychess";
+	sf::RenderWindow window(
+		sf::VideoMode(640,640),
+		 "Chess - start-up");
+	window.setFramerateLimit(60);
 
-	return0;
+	chess::engine::Board board;
+	chess::gui::Renderer renderer(80.0f);
+
+	while (window.isOpen()){
+		sf::Event ev;
+		while(window.pollEvent(ev)){
+			if (ev.type == sf::Event::Closed) window.close();
+			// TODO: drag_drop -> board.makeMove(...)
+		}
+
+		window.clear();
+		renderer.draw(window,board);
+		window.display();
+	}
+	return 0;
 
 }
