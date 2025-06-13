99d6679f04292eb4adfc17888f0341dfe5d71074
StudyKeepYell <skyjin6667@gmail.com>
2025-06-12 23:21:51 -0400

    ci fix

--- test/BoardTests.cpp
+++ test/BoardTests.cpp
@@ -1,4 +1,19 @@
 #include "engine/Board.hpp"
 #include <catch2/catch_test_macros.hpp> // ✅ Catch2 v3-style
 
-TEST_CASE("Intitial Board have 32 Pieces", "board") { chess::engine::Board b; }
+TEST_CASE("Intitial Board have 32 Pieces", "[board]") {
+  chess::engine::Board b;
+  int count = 0;
+  for (auto p : b.pieces())
+    if (p != chess::engine::PieceType::None)
+      ++count;
+
+  REQUIRE(count == 32);
+}
+
+TEST_CASE("King Square start at e1 , e8", "[board]") {
+  chess::engine::Board b;
+  using chess::engine::Color;
+  REQUIRE(b.kingSquare(Color::White) == 4);
+  REQUIRE(b.kingSquare(Color::Black) == 60);
+}
